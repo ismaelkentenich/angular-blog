82aea01bbd0e7c58eea218037732c1705e6c4524
Ismael Kentenich <ismaelkentenichm@gmail.com>
2024-01-24 16:50:29 -0300

    feat: using dataFake on home

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,13 +2,25 @@ export const dataFake = [
   {
     "id": "1",
     "image": "https://www.pulsecarshalton.co.uk/wp-content/uploads/2016/08/jk-placeholder-image.jpg",
-    "title": "novo album",
-    "description": "ariana",
+    "title": "Limpeza de primavera",
+    "description": "Dicas para uma limpeza de primavera eficaz e completa.",
   },
   {
     "id": "2",
     "image": "https://cinepop.com.br/wp-content/uploads/2024/01/ariana-grande-2.jpg.webp",
-    "title": "novo album",
-    "description": "ariana",
+    "title": "Manutenção de eletrodomésticos",
+    "description": "Guia passo a passo para manter seus eletrodomésticos funcionando bem.",
+  },
+  {
+    "id": "3",
+    "image": "https://www.pulsecarshalton.co.uk/wp-content/uploads/2016/08/jk-placeholder-image.jpg",
+    "title": "Jardinagem para iniciantes",
+    "description": "Tudo o que você precisa saber para começar a jardinagem em casa.",
+  },
+  {
+    "id": "4",
+    "image": "https://cinepop.com.br/wp-content/uploads/2024/01/ariana-grande-2.jpg.webp",
+    "title": "Decoração de interiores",
+    "description": "Inspiração e ideias para decorar sua casa com estilo e personalidade.",
   }
 ]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { dataFake } from '../../data/dataFake';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  articles = dataFake;
 }
